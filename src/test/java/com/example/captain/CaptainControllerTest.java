213513570afada21235172bf147ab62c8ff2930b
Anton Tarasov <atarasov@diasoft.ru>
2019-01-15 19:54:32 +0300

    Check rabbitmq message

--- src/test/java/com/example/captain/CaptainControllerTest.java
+++ src/test/java/com/example/captain/CaptainControllerTest.java
@@ -2,6 +2,7 @@ package com.example.captain;
 
 import com.example.captain.controller.CaptainController;
 import com.example.captain.domain.Captain;
+import com.example.captain.stream.CaptainStream;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
@@ -10,13 +11,16 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.Message;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,113 +29,126 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class CaptainControllerTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	public void crudTest() throws Exception {
-		// set captain
-		Captain create = new Captain(null, 1L, "Иванов Иван");
-		MockHttpServletRequestBuilder createBuilder =
-			MockMvcRequestBuilders.post("/v1/setCaptain/5")
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.accept(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(createBuilder)
-			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("\"success\":true")));
+    @Autowired
+    private CaptainStream stream;
 
-		// check captain
-		MockHttpServletRequestBuilder readBuilder =
-			MockMvcRequestBuilders.get("/v1/captain/5")
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.accept(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(readBuilder)
-			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("\"participantId\":1")))
-			.andExpect(content().string(containsString("\"participantIdentifier\":\"Иванов Иван\"")));
+    @Autowired
+    private MessageCollector messageCollector;
 
-		// remove captain
-		MockHttpServletRequestBuilder removeBuilder =
-			MockMvcRequestBuilders.post("/v1/removeCaptain/5")
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.accept(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(removeBuilder)
-			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("\"success\":true")));
+    @Test
+    public void crudTest() throws Exception {
+        // set captain
+        Captain create = new Captain(null, 1L, "Иванов Иван");
+        MockHttpServletRequestBuilder createBuilder =
+                MockMvcRequestBuilders.post("/v1/setCaptain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(createBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"success\":true")));
 
-		// check captain removed
-		MockHttpServletRequestBuilder readBuilder2 =
-			MockMvcRequestBuilders.get("/v1/captain/5")
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.accept(MediaType.APPLICATION_JSON)
-				.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(readBuilder2)
-			.andExpect(status().isOk())
-			.andExpect(content().string(containsString("\"exists\":false")));
-	}
+        // check message
+        Message<String> received = (Message<String>) messageCollector.forChannel(stream.outputStream()).poll();
+        assertEquals("{\"action\":\"setCaptain\",\"teamId\":5,\"participantId\":1,\"participantIdentifier\":\"Иванов Иван\"}", received.getPayload());
 
-	public void oneCaptainTest() throws Exception {
-		// set captain 1
-		Captain create = new Captain(null, 1L, "Иванов Иван");
-		MockHttpServletRequestBuilder createBuilder =
-				MockMvcRequestBuilders.post("/v1/setCaptain/5")
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.accept(MediaType.APPLICATION_JSON)
-						.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(createBuilder)
-				.andExpect(status().isOk())
-				.andExpect(content().string(containsString("\"success\":true")));
+        // check captain
+        MockHttpServletRequestBuilder readBuilder =
+                MockMvcRequestBuilders.get("/v1/captain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(readBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"participantId\":1")))
+                .andExpect(content().string(containsString("\"participantIdentifier\":\"Иванов Иван\"")));
 
-		// set captain 2 for same team
-		Captain create2 = new Captain(null, 2L, "Петров Петр");
-		MockHttpServletRequestBuilder createBuilder2 =
-				MockMvcRequestBuilders.post("/v1/setCaptain/5")
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.accept(MediaType.APPLICATION_JSON)
-						.content(new ObjectMapper().writeValueAsString(create2));
-		mvc.perform(createBuilder2)
-				.andExpect(status().isOk())
-				.andExpect(content().string(containsString("\"success\":false")));
+        // remove captain
+        MockHttpServletRequestBuilder removeBuilder =
+                MockMvcRequestBuilders.post("/v1/removeCaptain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(removeBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"success\":true")));
 
-		// check captain
-		MockHttpServletRequestBuilder readBuilder =
-				MockMvcRequestBuilders.get("/v1/captain/5")
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.accept(MediaType.APPLICATION_JSON)
-						.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(readBuilder)
-				.andExpect(status().isOk())
-				.andExpect(content().string(containsString("\"participantId\":1")))
-				.andExpect(content().string(containsString("\"participantIdentifier\":\"Иванов Иван\"")));
-	}
+        // check message
+        Message<String> received2 = (Message<String>) messageCollector.forChannel(stream.outputStream()).poll();
+        assertEquals("{\"action\":\"removeCaptain\",\"teamId\":5,\"participantId\":1,\"participantIdentifier\":\"Иванов Иван\"}", received2.getPayload());
 
-	public void otherTeamCaptainTest() throws Exception {
-		// set captain 1
-		Captain create = new Captain(null, 1L, "Иванов Иван");
-		MockHttpServletRequestBuilder createBuilder =
-				MockMvcRequestBuilders.post("/v1/setCaptain/5")
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.accept(MediaType.APPLICATION_JSON)
-						.content(new ObjectMapper().writeValueAsString(create));
-		mvc.perform(createBuilder)
-				.andExpect(status().isOk())
-				.andExpect(content().string(containsString("\"success\":true")));
+        // check captain removed
+        MockHttpServletRequestBuilder readBuilder2 =
+                MockMvcRequestBuilders.get("/v1/captain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(readBuilder2)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"exists\":false")));
+    }
 
-		// set captain 1 for other team
-		Captain create2 = new Captain(null, 1L, "Иванов Иван");
-		MockHttpServletRequestBuilder createBuilder2 =
-				MockMvcRequestBuilders.post("/v1/setCaptain/6")
-						.contentType(MediaType.APPLICATION_JSON_VALUE)
-						.accept(MediaType.APPLICATION_JSON)
-						.content(new ObjectMapper().writeValueAsString(create2));
-		mvc.perform(createBuilder2)
-				.andExpect(status().isOk())
-				.andExpect(content().string(containsString("\"success\":true")));
+    public void oneCaptainTest() throws Exception {
+        // set captain 1
+        Captain create = new Captain(null, 1L, "Иванов Иван");
+        MockHttpServletRequestBuilder createBuilder =
+                MockMvcRequestBuilders.post("/v1/setCaptain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(createBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"success\":true")));
 
-	}
+        // set captain 2 for same team
+        Captain create2 = new Captain(null, 2L, "Петров Петр");
+        MockHttpServletRequestBuilder createBuilder2 =
+                MockMvcRequestBuilders.post("/v1/setCaptain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create2));
+        mvc.perform(createBuilder2)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"success\":false")));
+
+        // check captain
+        MockHttpServletRequestBuilder readBuilder =
+                MockMvcRequestBuilders.get("/v1/captain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(readBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"participantId\":1")))
+                .andExpect(content().string(containsString("\"participantIdentifier\":\"Иванов Иван\"")));
+    }
+
+    public void otherTeamCaptainTest() throws Exception {
+        // set captain 1
+        Captain create = new Captain(null, 1L, "Иванов Иван");
+        MockHttpServletRequestBuilder createBuilder =
+                MockMvcRequestBuilders.post("/v1/setCaptain/5")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create));
+        mvc.perform(createBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"success\":true")));
+
+        // set captain 1 for other team
+        Captain create2 = new Captain(null, 1L, "Иванов Иван");
+        MockHttpServletRequestBuilder createBuilder2 =
+                MockMvcRequestBuilders.post("/v1/setCaptain/6")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(create2));
+        mvc.perform(createBuilder2)
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("\"success\":true")));
+
+    }
 
 }
-
